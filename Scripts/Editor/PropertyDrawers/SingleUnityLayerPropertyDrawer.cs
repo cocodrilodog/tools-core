efc2292e0a97e31c21d6f0733b2b425a06d787af
Juan Pablo Reyes Lega <juanpablo.reyes@cocodrilodog.com>
2019-11-20 11:13:51 -0500

    Added `SingleUnityLayer` and its property drawer.

---
+++ Scripts/Editor/PropertyDrawers/SingleUnityLayerPropertyDrawer.cs
@@ -0,0 +1,31 @@
+﻿namespace CocodriloDog.Core {
+
+	using UnityEditor;
+	using UnityEngine;
+
+	[CustomPropertyDrawer(typeof(SingleUnityLayer))]
+	public class SingleUnityLayerPropertyDrawer : PropertyDrawer {
+
+
+		#region Unity Methods
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+			EditorGUI.BeginProperty(position, GUIContent.none, property);
+			SerializedProperty layerIndex = property.FindPropertyRelative("m_LayerIndex");
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+			if (layerIndex != null) {
+				layerIndex.intValue = EditorGUI.LayerField(position, layerIndex.intValue);
+			}
+
+			EditorGUI.EndProperty();
+
+		}
+
+		#endregion
+
+
+	}
+
+}
---
+++ Scripts/SingleUnityLayer.cs
@@ -0,0 +1,40 @@
+﻿namespace CocodriloDog.Core {
+
+	using UnityEngine;
+
+	/// <summary>
+	/// Used as a Unity Layer Field where only one layer can be chosen.
+	/// </summary>
+	[System.Serializable]
+	public class SingleUnityLayer {
+
+
+		#region Public Properties
+
+		public int LayerIndex {
+			get { return m_LayerIndex; }
+			set {
+				if (value > 0 && value < 32) {
+					m_LayerIndex = value;
+				}
+			}
+		}
+
+		public int Mask {
+			get { return 1 << m_LayerIndex; }
+		}
+
+		#endregion
+
+
+		#region Private Fields
+
+		[SerializeField]
+		private int m_LayerIndex = 0;
+
+		#endregion
+
+
+	}
+
+}
