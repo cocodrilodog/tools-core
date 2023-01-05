03d4e20000722e1bf7bf12eabbbf959b0ab71eab
OMEN\juanp <juanpablo.reyes@cocodrilodog.com>
2023-01-04 22:59:06 -0500

    Updated documentation comment

--- Scripts/MonoScriptable/Editor/MonoScriptableObjectEditor.cs
+++ Scripts/MonoScriptable/Editor/MonoScriptableObjectEditor.cs
@@ -10,7 +10,7 @@
 	/// </summary>
 	/// 
 	/// <remarks>
-	/// It allows the user to go back and inspect to the owner object, as a  usability feature. 
+	/// It allows the user to go back and inspect the chain of owner objects, as a usability feature. 
 	/// It also disables the owner field so that it is not changed unintentionally.
 	/// </remarks>
     public abstract class MonoScriptableObjectEditor : Editor {
@@ -25,40 +25,12 @@
 		}
 
 		public override void OnInspectorGUI() {
-
 			serializedObject.Update();
-
 			DisabledField(ScriptProperty);
-
-			EditorGUILayout.BeginHorizontal();
-			var owner = (target as MonoScriptableObject).Owner;
-			var owners = new List<Object>();
-			while(owner != null) {
-				owners.Add(owner);
-				owner = owner is MonoScriptableObject ? (owner as MonoScriptableObject).Owner : null;
-			}
-			for(int i = owners.Count - 1; i >= 0; i--) {
-				if (GUILayout.Button($"◂ {owners[i].name}", GUILayout.ExpandWidth(false))) {
-					Selection.activeObject = owners[i];
-				}
-			}
-			EditorGUILayout.EndHorizontal();
+			DrawBreadcrumbs();
 			DisabledField(OwnerProperty);
-
 			EditorGUILayout.PropertyField(NameProperty);
-
 			serializedObject.ApplyModifiedProperties();
-
-			void DisabledField(SerializedProperty property, bool hasLabel = true) {
-				EditorGUI.BeginDisabledGroup(true);
-				if (hasLabel) {
-					EditorGUILayout.PropertyField(property);
-				} else {
-					EditorGUILayout.PropertyField(property, GUIContent.none);
-				}				
-				EditorGUI.EndDisabledGroup();
-			}
-
 		}
 
 		#endregion
@@ -71,6 +43,37 @@
 		private SerializedProperty OwnerProperty { get; set; }
 
 		private SerializedProperty NameProperty { get; set; }
+
+		#endregion
+
+
+		#region Private Methods
+
+		void DisabledField(SerializedProperty property, bool hasLabel = true) {
+			EditorGUI.BeginDisabledGroup(true);
+			if (hasLabel) {
+				EditorGUILayout.PropertyField(property);
+			} else {
+				EditorGUILayout.PropertyField(property, GUIContent.none);
+			}
+			EditorGUI.EndDisabledGroup();
+		}
+
+		private void DrawBreadcrumbs() {
+			EditorGUILayout.BeginHorizontal();
+			var owner = (target as MonoScriptableObject).Owner;
+			var owners = new List<Object>();
+			while (owner != null) {
+				owners.Add(owner);
+				owner = owner is MonoScriptableObject ? (owner as MonoScriptableObject).Owner : null;
+			}
+			for (int i = owners.Count - 1; i >= 0; i--) {
+				if (GUILayout.Button($"◂ {owners[i].name}", GUILayout.ExpandWidth(false))) {
+					Selection.activeObject = owners[i];
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+		}
 
 		#endregion
 
