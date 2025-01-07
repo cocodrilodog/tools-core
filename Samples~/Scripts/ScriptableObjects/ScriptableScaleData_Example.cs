0bac244096aa0496e1b743585239f60628ef8b2a
OMEN\juanp <juanpablo.reyes@cocodrilodog.com>
2025-01-07 05:54:25 -0500

    `ScriptableScaleData_Example`: Added create asset menu

--- Samples~/Scripts/ScriptableObjects/ScriptableScaleData_Example.cs
+++ Samples~/Scripts/ScriptableObjects/ScriptableScaleData_Example.cs
@@ -4,6 +4,7 @@ namespace CocodriloDog.Core.Examples {
 	using System.Collections.Generic;
 	using UnityEngine;
 
+	[CreateAssetMenu(menuName = "Cocodrilo Dog/Core/Examples/Scriptable Scale Data Example")]
 	public class ScriptableScaleData_Example : ScriptableValue<ScaleData_Example> { }
 
 }
