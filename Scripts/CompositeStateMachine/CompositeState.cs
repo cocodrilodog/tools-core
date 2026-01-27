35315fe9f61225b450f4a8d58b19060f16c6c00a
OMEN\juanp <juanpablo.reyes@cocodrilodog.com>
2026-01-27 21:16:29 +1100

    `CompositeState`: Added documentation comment.

--- Scripts/CompositeStateMachine/CompositeState.cs
+++ Scripts/CompositeStateMachine/CompositeState.cs
@@ -30,6 +30,17 @@ namespace CocodriloDog.Core {
 
 		public virtual void OnValidate() { }
 
+		/// <summary>
+		/// Invoked in addition to <see cref="OnValidate()"/> for cases where the validation
+		/// requires access to the state machine in edit mode
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// <paramref name="editModeMachine"/> is declared as <see cref="UnityEngine.Object"/>
+		/// because it can be a <see cref="MonoBehaviour"/> or a <see cref="ScriptableObject"/>.
+		/// </remarks>
+		/// 
+		/// <param name="editModeMachine">The state machine.</param>
 		public virtual void OnValidate(UnityEngine.Object editModeMachine) { }
 
 		public virtual void OnDestroy() { }
