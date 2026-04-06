aa489f48cef9600e176949de9b85a9101b3b8837
OMEN\juanp <juanpablo.reyes@cocodrilodog.com>
2026-04-06 20:09:39 +1000

    `MonoDecisionStateMachine`: m_Timed states.

--- Scripts/CompositeStateMachine/MonoDecisionStateMachine.cs
+++ Scripts/CompositeStateMachine/MonoDecisionStateMachine.cs
@@ -1,24 +1,75 @@
 namespace CocodriloDog.Core {
 
 	using System;
+	using System.Collections;
 	using System.Collections.Generic;
 	using System.Linq;
 	using UnityEngine;
 	using UnityEngine.Serialization;
 
 	/// <summary>
-	/// Concrete immplementation of <see cref="MonoDecisionStateMachineBase{T_State, T_Machine}"/>.
+	/// Concrete immplementation of <see cref="MonoDecisionStateMachineBase{T_State, T_Machine}"/>,
+	/// with states that can be timed.
 	/// </summary>
 	public class MonoDecisionStateMachine : MonoDecisionStateMachineBase<MonoDecisionStateMachine.State, MonoDecisionStateMachine> {
 
 
-		#region States
-
 		[Serializable]
-		public class State : DecisionStateBase<State, MonoDecisionStateMachine> { }
+		public class State : DecisionStateBase<State, MonoDecisionStateMachine> {
 
-		#endregion
 
+			#region Public Methods
+
+			public override void Enter() {
+				base.Enter();
+				if (m_IsTimed) {
+					m_DurationCoroutine = Machine.StartCoroutine(WaitForDuration());
+				}
+				IEnumerator WaitForDuration() {
+					yield return new WaitForSeconds(m_Duration);
+					Next();
+				}
+			}
+
+			public override void Exit() {
+				base.Exit();
+				if (m_DurationCoroutine != null) {
+					Machine.StopCoroutine(m_DurationCoroutine);
+					m_DurationCoroutine = null;
+				}
+			}
+
+			#endregion
+
+
+			#region Private Fields - Serialized
+
+			[SerializeField]
+			private bool m_IsTimed;
+
+			[Hide(nameof(HideDuration))]
+			[SerializeField]
+			private float m_Duration;
+
+			#endregion
+
+
+			#region Private Fields - Non Serialized
+
+			[NonSerialized]
+			private Coroutine m_DurationCoroutine;
+
+			#endregion
+
+
+			#region Private Methods
+
+			private bool HideDuration() => !m_IsTimed;
+
+			#endregion
+
+
+		}
 
 	}
 
